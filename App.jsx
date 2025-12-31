39a6f1e9c603d3cdb072a964ed2142a68f27a8d0
mahaboobgstp-hub <mahaboobgstp@gmail.com>
2025-12-31 12:21:34 +0530

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import { Routes, Route } from "react-router-dom";
+import PublicLayout from "./layouts/PublicLayout";
+import Home from "./public-pages/Home";
+
+export default function App() {
+  return (
+    <Routes>
+      <Route element={<PublicLayout />}>
+        <Route path="/" element={<Home />} />
+      </Route>
+    </Routes>
+  );
+}
