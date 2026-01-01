bb2cb2515c7842f5855bb5e17afb822d109a0631
mahaboobgstp-hub <mahaboobgstp@gmail.com>
2026-01-01 19:48:32 +0530

    Update B2BLayout.jsx

--- src/layouts/B2BLayout.jsx
+++ src/layouts/B2BLayout.jsx
@@ -1,1 +1,14 @@
+import Sidebar from "../components/b2b/Sidebar";
+import "../styles/b2b.css";
+
+export default function B2BLayout({ children }) {
+  return (
+    <div className="b2b-layout">
+      <Sidebar />
+      <main className="b2b-content">
+        {children}
+      </main>
+    </div>
+  );
+}
 
