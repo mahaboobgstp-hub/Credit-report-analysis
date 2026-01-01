bcae529d7da5b66c327938af9d17907477ab317d
mahaboobgstp-hub <mahaboobgstp@gmail.com>
2026-01-01 18:34:16 +0530

    Update DashboardLayout.jsx

--- src/layouts/DashboardLayout.jsx
+++ src/layouts/DashboardLayout.jsx
@@ -1,1 +1,12 @@
+import Sidebar from "../components/dashboard/Sidebar";
+import "../styles/dashboard.css";
+
+export default function DashboardLayout({ children }) {
+  return (
+    <div className="dashboard-layout">
+      <Sidebar />
+      <main className="dashboard-content">{children}</main>
+    </div>
+  );
+}
 
