f6dec6320ad3bfb706356b40b52bdddfaa2e87d5
mahaboobgstp-hub <mahaboobgstp@gmail.com>
2026-01-01 17:17:37 +0530

    Update Footer.jsx

--- src/common/Footer.jsx
+++ src/common/Footer.jsx
@@ -1,1 +1,16 @@
+import "../../styles/home.css";
+
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <p>
+        © {new Date().getFullYear()} MendCredit. All rights reserved.
+      </p>
+      <p className="footer-note">
+        MendCredit provides credit advisory and grievance support services.
+        We do not guarantee credit score improvement.
+      </p>
+    </footer>
+  );
+}
 
