52622795630978d3a98e94910e9ce6d50ee8d96d
mahaboobgstp-hub <mahaboobgstp@gmail.com>
2026-01-01 17:16:05 +0530

    Update CTA.jsx

--- src/components/home/CTA.jsx
+++ src/components/home/CTA.jsx
@@ -1,1 +1,19 @@
+import "../../styles/home.css";
+
+export default function CTA() {
+  return (
+    <section className="cta">
+      <h2>Take Control of Your Credit Today</h2>
+      <p>
+        If your credit report has errors, delays, or unfair markings,
+        the sooner you act, the better your financial future.
+      </p>
+
+      <div className="cta-buttons">
+        <button className="primary">Check My Credit Issue</button>
+        <button className="secondary">Speak to an Expert</button>
+      </div>
+    </section>
+  );
+}
 
