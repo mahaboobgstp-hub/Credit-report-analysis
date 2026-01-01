b4ec88ec290b9aee04450a290c1ac5ddffac8385
mahaboobgstp-hub <mahaboobgstp@gmail.com>
2026-01-01 20:31:09 +0530

    Update LSPLayout.jsx

--- src/layouts/LSPLayout.jsx
+++ src/layouts/LSPLayout.jsx
@@ -1,1 +1,12 @@
+import "../styles/lsp.css";
+import ProgressStepper from "../components/lsp/ProgressStepper";
+
+export default function LSPLayout({ step, children }) {
+  return (
+    <div className="lsp-container">
+      <ProgressStepper currentStep={step} />
+      <div className="lsp-content">{children}</div>
+    </div>
+  );
+}
 
